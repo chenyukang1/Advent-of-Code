50a61def1d41b1d5170fa28563c89d14cc6bbd77
yukang.chen <chen.yukang@qq.com>
2026-01-08 15:10:23 +0800

    2025 day 4

---
+++ 2025/day4/part1.py
@@ -0,0 +1,31 @@
+def rolls_of_paper() -> int:
+    ans = 0
+    rolls = []
+    with open('2025/day4/input.txt', 'r') as f:
+        for line in f:
+            rolls.append(list(line.strip()))
+    dirs = [[-1, 0], [-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1]]
+    rows, cols = len(rolls), len(rolls[0])
+    for i, row in enumerate(rolls):
+        for j, letter in enumerate(row):
+            if letter != '@':
+                continue
+            count = 0
+            for dir in dirs:
+                newY = i + dir[0]
+                newX = j + dir[1]
+                if (newY >= 0 and newY < rows and newX >=0 and newX < cols
+                    and rolls[newY][newX] == '@'):
+                    count += 1
+            if count < 4:
+                print(f'found at {i} {j}')
+                ans += 1
+
+    return ans
+
+
+def main():
+    print(rolls_of_paper())
+
+if __name__ == "__main__":
+    main()
